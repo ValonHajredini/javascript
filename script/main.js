fdd6edfb48fe7949c10ea5d4f855838c8c3a5436
Valon Hajredini <hajredini.valon@gmail.com>
2016-02-22 16:59:52 +0100

    22. Operator Precedence and Associatinity

--- script/main.js
+++ script/main.js
@@ -1,7 +1,18 @@
 //-----------------------------------------------------------------
-//21. Conceptual Aside
-var a = 3 > 4;
+// 22. Operator Precedence and Associatinity
+
+var aa = 3 + 4 * 5;
+console.log(aa);
+var a = 2, b = 3, c = 4;
+a = b = c;
 console.log(a);
+console.log(b);
+console.log(c);
+
+//-----------------------------------------------------------------
+//21. Conceptual Aside
+//var a = 3 > 4;
+//console.log(a);
 
 
 

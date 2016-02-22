19d909d1a8fb04087305efc8914722a5b04d088e
Valon Hajredini <hajredini.valon@gmail.com>
2016-02-22 11:26:07 +0100

    16. The Scope Chain

--- script/main.js
+++ script/main.js
@@ -1,22 +1,35 @@
-//----------------------------------------------------------------------
-
-// Lesen 15 Functions, Context and Variable
+//-----------------------------------------------------------------
+//16. The Scope Chain
 function b(){
-    var myVar;
     console.log(myVar);
 }
 function a(){
-    var myVar = 2;
-    console.log(myVar);
+
+    var myVar =2;
     b();
 }
 var myVar = 1;
-
-console.log(myVar);
 a();
-console.log(myVar);
 
-//----------------------------------------------------------------------
+//----------------------------------------------------------------
+
+// Lesen 15 Functions, Context and Variable
+//function b(){
+//    var myVar;
+//    console.log(myVar);
+//}
+//function a(){
+//    var myVar = 2;
+//    console.log(myVar);
+//    b();
+//}
+//var myVar = 1;
+//
+//console.log(myVar);
+//a();
+//console.log(myVar);
+
+//---------------------------------------------------------------
 
 // Lesen 14.
 //function b(){
@@ -29,4 +42,4 @@ console.log(myVar);
 //    b();
 //};
 //a();
-//----------------------------------------------------------------------
+//----------------------------------------------------------------

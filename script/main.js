b43aee3360b5d0f9d3c02393580260176fc36d55
Valon Hajredini <hajredini.valon@gmail.com>
2016-03-06 23:45:55 +0100

    52. Fun programing part 2

--- script/main.js
+++ script/main.js
@@ -1,45 +1,51 @@
 //-----------a------------------------------------------------------
-//51. Functional Programing
-
-
-function mapForEach(arr,fn){
-    var newArr = [];
-    for(var i = 0; i< arr.length ; i++){
-        newArr.push(
-            fn(arr[i])
-        )
-    };
-    return newArr;
-}
+//52. Fun programing part 2
 var arr1 = [1,2,3]
-console.log(arr1);
-var arr2 = mapForEach(arr1, function(item){
-   return item*2;
-});
-//for (var i=0 ; i < arr1.length; i++){
-//    arr2.push(arr1[i]*2);
+var arr6 = _.map(arr1, function (item){ return item * 3});
+console.log(arr6);
+var arr7 = _.filter([2,3,4,5,6,7], function(item){return item % 2 === 0})
+console.log(arr7);
+//-----------a------------------------------------------------------
+//51. Functional Programing part 1
+
+//function mapForEach(arr,fn){
+//    var newArr = [];
+//    for(var i = 0; i< arr.length ; i++){
+//        newArr.push(
+//            fn(arr[i])
+//        )
+//    };
+//    return newArr;
 //}
-console.log(arr2);
-
-var arr3 = mapForEach(arr1, function(item){
-    return "{"+item+ "}";
-});
-console.log(arr3);
-
-
-var checkPastLimit = function(limiter, item){
-    return item > limiter;
-}
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
-console.log(arr4);
-
-var checkLimitPastSimplefide = function (limiter){
-    return function(limiter, item){
-        return item > limiter;
-    }.bind(this, limiter);
-};
-var arr5 = mapForEach(arr1, checkLimitPastSimplefide(3))
-console.log(arr5);
+//var arr1 = [1,2,3]
+//console.log(arr1);
+//var arr2 = mapForEach(arr1, function(item){
+//   return item*2;
+//});
+////for (var i=0 ; i < arr1.length; i++){
+////    arr2.push(arr1[i]*2);
+////}
+//console.log(arr2);
+//
+//var arr3 = mapForEach(arr1, function(item){
+//    return "{"+item+ "}";
+//});
+//console.log(arr3);
+//
+//
+//var checkPastLimit = function(limiter, item){
+//    return item > limiter;
+//}
+//var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+//console.log(arr4);
+//
+//var checkLimitPastSimplefide = function (limiter){
+//    return function(limiter, item){
+//        return item > limiter;
+//    }.bind(this, limiter);
+//};
+//var arr5 = mapForEach(arr1, checkLimitPastSimplefide(3))
+//console.log(arr5);
 
 //-----------a------------------------------------------------------
 //50. call(), apply(), bind()
